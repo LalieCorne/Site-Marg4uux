8fb75aa3b3b6a1346c1eaed0321ab1b29d9c5cd6
LalieCorne <blacklina4@gmail.com>
2022-02-15 15:06:08 +0100

    footer done

--- partials/footer.php
+++ partials/footer.php
@@ -3,7 +3,8 @@
 <footer class="footer mt-auto py-3 bg-light">
     <div class="container">
         <span class="text-muted">
-            <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/aldo-cervantes" title="Aldo Cervantes">Aldo Cervantes</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div></span>
+            <div>Site web créé par Lalie_Corne</div>
+            <div>Icones crées par <a href="https://www.freepik.com" title="Freepik">Freepik</a> et <a href="https://www.flaticon.com/authors/aldo-cervantes" title="Aldo Cervantes">Aldo Cervantes</a> depuis <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div></span>
     </div>
 </footer>
 
